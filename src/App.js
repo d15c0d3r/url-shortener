68b8af70e01274bc96601bfc9e3bed5ed1091ba7
thatviper <challanaganarasimhareddy@gmail.com>
2021-11-14 11:37:01 +0530

    .env, boilerplate

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { BrowserRouter } from "react-router-dom";
+import {Route , Switch} from "react-router-dom";
+import { useState } from "react";
+
 function App() {
+  const [url, setUrl] = useState("");
+
   return (
     <div>
-      hello
+      <label>Enter URL to Shorten </label>
+      <input 
+        required
+        id = "url" 
+        type = "text" 
+        onChange = {(e)=>{setUrl(e.target.value)}}/>
+        <br/>
+      <button>Shorten!</button>
     </div>
   );
 }
